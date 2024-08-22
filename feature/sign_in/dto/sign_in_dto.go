d291a87310ed4bc94957dd19303045b7b197f45b
PhamVietHoang <vhoang1928@gmail.com>
2024-08-22 14:37:31 +0700

    update cmt dto

--- feature/sign_in/dto/sign_in_dto.go
+++ feature/sign_in/dto/sign_in_dto.go
@@ -1,12 +1,14 @@
 package dto
 
 // SignInRequestDTO is used to capture the data required for a user sign-in request.
+// SignInRequestDTO được sử dụng để thu thập dữ liệu cần thiết cho yêu cầu đăng nhập của người dùng.
 type SignInRequestDTO struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
 }
 
 // SignInResponseDTO is used to send back a response after a successful sign-in.
+// SignInResponseDTO được sử dụng để gửi lại phản hồi sau khi đăng nhập thành công.
 type SignInResponseDTO struct {
 	ID        uint   `json:"id"`
 	Username  string `json:"username"`
@@ -18,6 +20,7 @@ type SignInResponseDTO struct {
 }
 
 // SignUpRequestDTO is used to capture the data required for a user sign-up request.
+// SignUpRequestDTO được sử dụng để thu thập dữ liệu cần thiết cho yêu cầu đăng ký của người dùng.
 type SignUpRequestDTO struct {
 	Username string `json:"username" binding:"required"`
 	Password string `json:"password" binding:"required"`
@@ -26,6 +29,7 @@ type SignUpRequestDTO struct {
 }
 
 // SignUpResponseDTO is used to send back a response after a successful sign-up.
+// SignUpResponseDTO được sử dụng để gửi lại phản hồi sau khi đăng ký thành công.
 type SignUpResponseDTO struct {
 	ID        uint   `json:"id"`
 	Username  string `json:"username"`
